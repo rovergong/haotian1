5ff8fdc4a30c041f30c1f49f949590f005098789
rovergong <317907660@qq.com>
2024-11-30 12:28:36 +0800

    edit the device

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_haotian-user
-add_lunch_combo omni_haotian-userdebug
-add_lunch_combo omni_haotian-eng
+add_lunch_combo twrp_haotian-user
+add_lunch_combo twrp_haotian-userdebug
+add_lunch_combo twrp_haotian-eng
